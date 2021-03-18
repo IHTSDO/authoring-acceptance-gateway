a1b952f55772e93569a5512707092242b03bcb69
Kai Kewley <kaikewley@gmail.com>
2021-03-18 17:07:16 +0000

    FRI-45 Add '/version' controller.

---
+++ src/main/java/org/snomed/aag/rest/VersionController.java
@@ -0,0 +1,28 @@
+package org.snomed.aag.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.snomed.aag.rest.pojo.BuildVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "Version", description = "Build Version")
+public class VersionController {
+
+	@Autowired(required = false)
+	private BuildProperties buildProperties;
+
+	@ApiOperation("Software build version and timestamp.")
+	@RequestMapping(value = "/version", method = RequestMethod.GET, produces = "application/json")
+	public BuildVersion getBuildInformation() {
+	    if (buildProperties == null) {
+	        throw new IllegalStateException("Build properties are not present.");
+        }
+		return new BuildVersion(buildProperties.getVersion(), buildProperties.getTime().toString());
+	}
+
+}
