29fbbe7b8624b6c8062d3efb78e66266e8977277
Darryn McGaw <dmcg@ihtsdo.org>
2021-03-25 17:01:41 +0000

    FRI-48 Check access to Snowstorm on startup

---
+++ src/main/java/org/snomed/aag/SnowstormAccessValidator.java
@@ -0,0 +1,37 @@
+package org.snomed.aag;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@ConditionalOnProperty(name = "snowstorm.confirm-access", havingValue = "true")
+@Component
+public class SnowstormAccessValidator implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SnowstormAccessValidator.class);
+
+    @Value("${snowstorm.url}")
+    private String snowstormUrl;
+
+    private final RestTemplate restTemplate;
+
+    public SnowstormAccessValidator(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public void run(String... args) {
+        LOGGER.info("Confirming access to Snowstorm...");
+        try {
+            this.restTemplate.getForObject(snowstormUrl, String.class);
+            LOGGER.info("Successfully confirmed access to Snowstorm.");
+        } catch (Exception e) {
+            LOGGER.warn("Cannot access Snowstorm at {}. Is Snowstorm accessible?", snowstormUrl);
+        }
+    }
+
+}
