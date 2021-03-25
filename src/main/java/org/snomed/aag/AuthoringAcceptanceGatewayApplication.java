79bcd78bc1aa449349e8b7f65ca47cd7914035a9
Darryn McGaw <dmcg@ihtsdo.org>
2021-03-25 16:58:51 +0000

    FRI-48 Rename main class

--- src/main/java/org/snomed/aag/AuthoringAcceptanceGatewayApplication.java
+++ src/main/java/org/snomed/aag/AuthoringAcceptanceGatewayApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-public class ServiceAcceptanceServiceApplication extends Config {
+public class AuthoringAcceptanceGatewayApplication extends Config {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceAcceptanceServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthoringAcceptanceGatewayApplication.class, args);
+    }
 
 }
