22cca126252c0b2ea767d18ff48f1c149e1cbd85
Darryn McGaw <dmcg@ihtsdo.org>
2021-04-07 17:58:53 +0100

    FRI-49 Change CriteriaItemSignOffService.findAllByBranchAndIdentifier method to use repository instead of template
    This is now possible because some of CriteriaItemSignOff's FieldTypes were changed to Keyword

--- src/main/java/org/snomed/aag/data/repositories/CriteriaItemSignOffRepository.java
+++ src/main/java/org/snomed/aag/data/repositories/CriteriaItemSignOffRepository.java
@@ -3,7 +3,9 @@ package org.snomed.aag.data.repositories;
 import org.snomed.aag.data.domain.CriteriaItemSignOff;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 
 public interface CriteriaItemSignOffRepository extends ElasticsearchRepository<CriteriaItemSignOff, String> {
-
+    List<CriteriaItemSignOff> findAllByBranchAndCriteriaItemIdIn(String branch, List<String> criteriaItemIdentifiers);
 }
--- src/main/java/org/snomed/aag/data/services/CriteriaItemSignOffService.java
+++ src/main/java/org/snomed/aag/data/services/CriteriaItemSignOffService.java
@@ -2,22 +2,15 @@ package org.snomed.aag.data.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.snomed.aag.data.Constants;
 import org.snomed.aag.data.domain.CriteriaItem;
 import org.snomed.aag.data.domain.CriteriaItemSignOff;
 import org.snomed.aag.data.repositories.CriteriaItemSignOffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.SearchHitsIterator;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static org.elasticsearch.index.query.QueryBuilders.*;
 
 @Service
 public class CriteriaItemSignOffService {
@@ -25,9 +18,6 @@ public class CriteriaItemSignOffService {
 
     @Autowired
     private CriteriaItemSignOffRepository repository;
-
-    @Autowired
-    private ElasticsearchRestTemplate elasticsearchTemplate;
 
     public CriteriaItemSignOff create(CriteriaItemSignOff criteriaItemSignOff) {
         LOGGER.info("{} signing off {} for {}.", criteriaItemSignOff.getUserId(), criteriaItemSignOff.getCriteriaItemId(), criteriaItemSignOff.getBranch());
@@ -43,25 +33,11 @@ public class CriteriaItemSignOffService {
      * @return Collection of matched CriteriaItemSign.
      */
     public List<CriteriaItemSignOff> findAllByBranchAndIdentifier(String branchPath, Set<CriteriaItem> criteriaItems) {
-        //Collect all identifiers
         List<String> criteriaItemIdentifiers = criteriaItems.stream().map(CriteriaItem::getId).collect(Collectors.toList());
         LOGGER.info("Checking whether Criteria Items {} have been completed on branch {}.", criteriaItemIdentifiers, branchPath);
 
-        //Build & run query, then collect results
-        List<CriteriaItemSignOff> results = new ArrayList<>();
-        try (SearchHitsIterator<CriteriaItemSignOff> hits = elasticsearchTemplate.searchForStream(new NativeSearchQueryBuilder()
-                .withQuery(
-                        boolQuery()
-                                .must(matchQuery(CriteriaItemSignOff.Fields.BRANCH, branchPath))
-                                .must(termsQuery(CriteriaItemSignOff.Fields.CRITERIA_ITEM_ID, criteriaItemIdentifiers))
-                )
-                .withPageable(Constants.LARGE_PAGE)
-                .build(), CriteriaItemSignOff.class)) {
-            hits.forEachRemaining(hit -> results.add(hit.getContent()));
-        }
-
-        //Mark any Criteria Item as complete if there is a match
-        for (CriteriaItemSignOff criteriaItemSignOff : results) {
+        List<CriteriaItemSignOff> criteriaItemSignOffs = repository.findAllByBranchAndCriteriaItemIdIn(branchPath, criteriaItemIdentifiers);
+        for (CriteriaItemSignOff criteriaItemSignOff : criteriaItemSignOffs) {
             String criteriaItemSignOffId = criteriaItemSignOff.getCriteriaItemId();
             for (CriteriaItem criteriaItem : criteriaItems) {
                 if (criteriaItem.getId().equals(criteriaItemSignOffId)) {
@@ -70,7 +46,7 @@ public class CriteriaItemSignOffService {
             }
         }
 
-        LOGGER.debug("Branch {} has {} completed Criteria Items.", branchPath, results.size());
-        return results;
+        LOGGER.debug("Branch {} has {} completed Criteria Items.", branchPath, criteriaItemSignOffs.size());
+        return criteriaItemSignOffs;
     }
 }
