3c023ae394a45f7cf55d803f62a5e71d78783ce7
Darryn McGaw <dmcg@ihtsdo.org>
2021-04-07 10:18:55 +0100

    FRI-49 Add JsonIgnore annotation to getAllCriteriaIdentifiers method in ProjectAcceptanceCriteria

--- src/main/java/org/snomed/aag/data/domain/ProjectAcceptanceCriteria.java
+++ src/main/java/org/snomed/aag/data/domain/ProjectAcceptanceCriteria.java
@@ -1,5 +1,6 @@
 package org.snomed.aag.data.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -56,6 +57,7 @@ public class ProjectAcceptanceCriteria {
 		this.selectedTaskCriteriaIds = selectedTaskCriteriaIds;
 	}
 
+	@JsonIgnore
 	public Set<String> getAllCriteriaIdentifiers() {
 		Set<String> identifiers = new HashSet<>();
 		identifiers.addAll(selectedProjectCriteriaIds);
