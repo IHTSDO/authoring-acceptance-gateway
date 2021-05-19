54e9ac972e1adde2bfea7b1a1c2b2b1035e7b72f
Darryn McGaw <dmcg@ihtsdo.org>
2021-05-19 16:11:04 +0100

    FRI-48 Add standalone validator class for ProjectAcceptanceCriteria

---
+++ src/test/java/org/snomed/aag/data/validators/ProjectAcceptanceCriteriaValidatorTest.java
@@ -0,0 +1,46 @@
+package org.snomed.aag.data.validators;
+
+import org.junit.jupiter.api.Test;
+import org.snomed.aag.data.domain.ProjectAcceptanceCriteria;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+class ProjectAcceptanceCriteriaValidatorTest {
+    private final ElasticsearchRestTemplate elasticsearchRestTemplate = mock(ElasticsearchRestTemplate.class);
+    private final ProjectAcceptanceCriteriaValidator target = new ProjectAcceptanceCriteriaValidator(elasticsearchRestTemplate);
+
+    @Test
+    void validate_ShouldThrowException_WhenGivenNull() {
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            // when
+            target.validate(null);
+        });
+    }
+
+    @Test
+    void validate_ShouldThrowException_WhenMissingProjectIteration() {
+        // given
+        ProjectAcceptanceCriteria projectAcceptanceCriteria = new ProjectAcceptanceCriteria("MAIN");
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            // when
+            target.validate(projectAcceptanceCriteria);
+        });
+    }
+
+    @Test
+    void validate_ShouldThrowException_WhenProjectIterationIsNegative() {
+        // given
+        ProjectAcceptanceCriteria projectAcceptanceCriteria = new ProjectAcceptanceCriteria("MAIN", -1);
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            // when
+            target.validate(projectAcceptanceCriteria);
+        });
+    }
+}
