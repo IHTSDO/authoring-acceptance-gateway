9059d2b0942107718fd5578a1b88f9093935000c
Darryn McGaw <dmcg@ihtsdo.org>
2021-05-28 17:21:01 +0100

    FRI-100 Add SMALL_PAGE to Constants

--- src/main/java/org/snomed/aag/data/Constants.java
+++ src/main/java/org/snomed/aag/data/Constants.java
@@ -4,5 +4,6 @@ import org.springframework.data.domain.PageRequest;
 
 public class Constants {
 	public static final PageRequest PAGE_OF_ONE = PageRequest.of(0, 1);
+	public static final PageRequest SMALL_PAGE = PageRequest.of(0, 100);
 	public static final PageRequest LARGE_PAGE = PageRequest.of(0, 10_000);
 }
