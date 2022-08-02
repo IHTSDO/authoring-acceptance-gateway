7aeef9a54cd6a266182cc136294c6ab13ca87879
Elena Ilyukhina <eil@ihtsdo.org>
2022-08-02 18:13:44 +0100

    FRI-476 - Updated Spring Security filters; added AccessDeniedExceptionHandler

--- src/main/java/org/snomed/aag/config/SecurityAndSwaggerConfig.java
+++ src/main/java/org/snomed/aag/config/SecurityAndSwaggerConfig.java
@@ -1,17 +1,19 @@
 package org.snomed.aag.config;
 
 import org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator;
-import org.snomed.aag.rest.security.RequestHeaderAuthenticationDecoratorWithRequiredRole;
+import org.snomed.aag.rest.security.AccessDeniedExceptionHandler;
+import org.snomed.aag.rest.security.RequiredRoleFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -31,6 +33,17 @@ public class SecurityAndSwaggerConfig extends WebSecurityConfigurerAdapter {
 	@Autowired(required = false)
 	private BuildProperties buildProperties;
 
+	@Value("${ims-security.required-role}")
+	private String requiredRole;
+
+	private final String[] excludedUrlPatterns = {
+			"/version",
+			"/swagger-ui.html",
+			"/swagger-resources/**",
+			"/v2/api-docs",
+			"/webjars/springfox-swagger-ui/**"
+	};
+
 	@Bean
 	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
 		DefaultHttpFirewall firewall = new DefaultHttpFirewall();
@@ -47,9 +60,15 @@ public class SecurityAndSwaggerConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();// lgtm [java/spring-disabled-csrf-protection]
 
-		http
-				.authorizeRequests()
-				.anyRequest().permitAll();
+		http.addFilterAfter(new RequestHeaderAuthenticationDecorator(), BasicAuthenticationFilter.class);
+		http.addFilterAt(new RequiredRoleFilter(requiredRole, excludedUrlPatterns), FilterSecurityInterceptor.class);
+
+		http.authorizeRequests()
+				.antMatchers(excludedUrlPatterns).permitAll()
+				.anyRequest().authenticated()
+				// Handles AccessDeniedException thrown within Spring Security filter chain, i.e. before the request reaches any controller
+				.and().exceptionHandling().accessDeniedHandler(new AccessDeniedExceptionHandler())
+				.and().httpBasic();
 	}
 
 	@Bean
@@ -57,8 +76,13 @@ public class SecurityAndSwaggerConfig extends WebSecurityConfigurerAdapter {
 	public Docket api() {
 		Docket docket = new Docket(DocumentationType.SWAGGER_2);
 		final String version = buildProperties != null ? buildProperties.getVersion() : "DEV";
-		docket.apiInfo(new ApiInfo("Authoring Acceptance Gateway", "Microservice to ensure service acceptance criteria are met before content promotion within the SNOMED CT Authoring Platform.", version, null,
-				new Contact("SNOMED International", "https://github.com/IHTSDO/authoring-acceptance-gateway", null), "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0"));
+		docket.apiInfo(new ApiInfo(
+				"Authoring Acceptance Gateway",
+				"Microservice to ensure service acceptance criteria are met before content promotion within the SNOMED CT Authoring Platform.",
+				version,
+				null,
+				new Contact("SNOMED International", "https://github.com/IHTSDO/authoring-acceptance-gateway", null),
+				"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0"));
 		ApiSelectorBuilder apiSelectorBuilder = docket.select();
 
 		apiSelectorBuilder
@@ -70,24 +94,6 @@ public class SecurityAndSwaggerConfig extends WebSecurityConfigurerAdapter {
 				.paths(not(regex("/")));
 
 		return apiSelectorBuilder.build();
-	}
-
-	@Bean
-	public FilterRegistrationBean<RequestHeaderAuthenticationDecorator> getSingleSignOnFilter() {
-		FilterRegistrationBean<RequestHeaderAuthenticationDecorator> filterRegistrationBean = new FilterRegistrationBean<>(
-				new RequestHeaderAuthenticationDecorator());
-		filterRegistrationBean.setOrder(1);
-		return filterRegistrationBean;
-	}
-
-	@Bean
-	public FilterRegistrationBean<RequestHeaderAuthenticationDecoratorWithRequiredRole> getRequiredRoleFilter(@Value("${ims-security.required-role}") String requiredRole) {
-		FilterRegistrationBean<RequestHeaderAuthenticationDecoratorWithRequiredRole> filterRegistrationBean = new FilterRegistrationBean<>(
-				new RequestHeaderAuthenticationDecoratorWithRequiredRole(requiredRole)
-						.addExcludedPath("/webjars/springfox-swagger-ui")
-		);
-		filterRegistrationBean.setOrder(2);
-		return filterRegistrationBean;
 	}
 
 }
