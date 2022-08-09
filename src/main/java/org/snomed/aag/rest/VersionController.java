b6dbe979293a2cef778a3dbb7379248aab95cbfb
Elena Ilyukhina <eil@ihtsdo.org>
2022-08-09 10:36:04 +0100

    FRI-476 - Deleted unused imports

--- src/main/java/org/snomed/aag/rest/VersionController.java
+++ src/main/java/org/snomed/aag/rest/VersionController.java
@@ -6,8 +6,6 @@ import org.snomed.aag.rest.pojo.BuildVersion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
